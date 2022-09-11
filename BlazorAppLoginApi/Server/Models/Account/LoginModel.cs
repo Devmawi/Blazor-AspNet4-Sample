2cd65aef61e9984c7b351335d4484034b52593be
dev <dev@dev.org>
2022-09-11 18:55:02 +0200

    add antiforgery token

--- BlazorAppLoginApi/Server/Models/Account/LoginModel.cs
+++ BlazorAppLoginApi/Server/Models/Account/LoginModel.cs
@@ -1,8 +1,13 @@
-﻿namespace BlazorAppLoginApi.Server.Models.Account
+﻿using System.ComponentModel;
+
+namespace BlazorAppLoginApi.Server.Models.Account
 {
     public class LoginModel
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        [DisplayName("__RequestVerificationToken")]
+        public string __RequestVerificationToken { get; set; }
     }
 }
